0d97f5a4d972eed8781bb32e10f6f3bfcf4fbf3e
ryalo <45079717+ryalo@users.noreply.github.com>
2018-11-15 23:36:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const prefix = "!"
 
 client.on('ready', () => {
    console.log(`----------------`);
